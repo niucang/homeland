394ee98ef116e276248f214c4c386cdc37b0f3e7
niucang <1402703522@qq.com>
2018-07-20 13:11:38 +0800

    修复手机号

--- app/services/msg_code_service.rb
+++ app/services/msg_code_service.rb
@@ -3,8 +3,9 @@ class MsgCodeService
     def send_code_and_cache_sms(mobile_phone)
       cache_key = mobile_phone_cache_key(mobile_phone)
       sms_code = $redis.get(cache_key).presence || get_code
-      $redis.set(cache_key, sms_code, ex: 2 * 60 * 60)
-      # Aliyun::Sms.send(mobile_phone, 'template_code', {sms_code: sms_code}, '')
+      $redis.set(cache_key, sms_code, ex: 5 * 60)
+      Rails.logger.info("SEND_SMS_139971832: code#{sms_code}")
+      Aliyun::Sms.send(mobile_phone, 'SMS_139971832', {code: sms_code}.to_json)
     end
 
     def verify_message_code(mobile_phone, sms_code)
