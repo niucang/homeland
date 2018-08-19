82033a5b36c6bbcf967af5dd85f65a3cc6eb5c2b
niucang <1402703522@qq.com>
2018-08-19 12:25:27 +0800

    fix regexp

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -113,7 +113,7 @@ class Topic < ApplicationRecord
   end
 
   def images
-    body.scan(/!\[\]\(([^!\[\]]*)\)/).map(&:first)
+    body.scan(/!\[\]\(([^!\[\]]*)[^\(\p{Han}]\)/).map(&:first)
   end
 
   def method_name
